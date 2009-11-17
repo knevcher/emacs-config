74b19136dc6671fbf23fb6842716af6b82493c8d
knevcher <knevcher@knevcher-desktop.(none)>
2009-11-17 09:40:53 +0300

    -- Удалил yasnippet - перенес его в elpa

--- setup/yasnippet-setup.el
+++ setup/yasnippet-setup.el
@@ -1,7 +1,9 @@
-(add-to-list 'load-path "~/.emacs.d/modules/yasnippet/")
+(add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-bundle-0.6.1/")
 
-(require 'yasnippet-bundle)
-(yas/initialize)
-(yas/load-directory "~/.emacs.d/modules/yasnippet")
+(load "~/.emacs.d/elpa/yasnippet-bundle-0.6.1/yasnippet-bundle-autoloads.el")
+(provide 'yasnippet-bundle-autoloads)
+;;(require 'yasnippet-bundle)
+;;(yas/initialize)
+;;(yas/load-directory "~/.emacs.d/modules/yasnippet")
 ;; I don't like using partial words for completion
 (setq yas/key-syntaxes '("w_" "w_." "^ "))
