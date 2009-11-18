fac7bca5985d81efe2cbbc52b349b88b1339691f
knevcher <knevcher@knevcher-desktop.(none)>
2009-11-18 09:53:18 +0300

    --

--- setup/yasnippet-setup.el
+++ setup/yasnippet-setup.el
@@ -1,7 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-bundle-0.6.1/")
 
 (load "~/.emacs.d/elpa/yasnippet-bundle-0.6.1/yasnippet-bundle-autoloads.el")
-(provide 'yasnippet-bundle-autoloads)
+(require 'yasnippet-bundle-autoloads)
 ;;(require 'yasnippet-bundle)
 ;;(yas/initialize)
 ;;(yas/load-directory "~/.emacs.d/modules/yasnippet")
