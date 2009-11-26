79befd51e98abace3a20fd1725f69164c9928928
knevcher <knevcher@knevcher-desktop.(none)>
2009-11-26 10:47:33 +0300

    -- updated ido setup

--- setup/ido-filecache-setup.el
+++ setup/ido-filecache-setup.el
@@ -6,11 +6,11 @@
 ;;(ido-everywhere 1)
 (setq ido-enable-flex-matching t)
 (setq ido-create-new-buffer 'always)
-(setq ido-enable-tramp-completion nil)
+(setq ido-enable-tramp-completion t)
 (setq ido-enable-last-directory-history nil)
 (setq ido-confirm-unique-completion nil) ;; wait for RET, even for unique?
 (setq ido-show-dot-for-dired t) ;; put . as the first item
-(setq ido-use-filename-at-point t) ;; prefer file names near point
+;;(setq ido-use-filename-at-point t) ;; prefer file names near point
 
 
 (defun file-cache-ido-find-file (file)
