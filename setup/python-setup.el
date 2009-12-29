b34117cd7cb70f73355c107a92c98e6e60faf97b
Knyazev Andrey <knevcher@gmail.com>
2009-12-29 14:36:00 +0300

    -- added rope, pylint -- added auto-complete -- added to color themes

--- setup/python-setup.el
+++ setup/python-setup.el
@@ -1,11 +1,15 @@
 (add-to-list 'load-path "~/.emacs.d/modules/python/")
-
+(autoload 'python-mode "python-mode" "Python Mode." t)
 (require 'python-mode)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
-;;(require 'pymacs)
-;;(pymacs-load "ropemacs" "rope-")
+(require 'python-init)
+(require 'ipython)
+(setq py-python-command-args '( "-colors" "Linux"))
+(defadvice py-execute-buffer (around python-keep-focus activate)
+"return focus to python code buffer"
+(save-excursion ad-do-it))
 
-(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-(setq interpreter-mode-alist (cons '("python" . python-mode)
-                                   interpreter-mode-alist))
-(autoload 'python-mode "python-mode" "Python editing mode." t)
+(load-library "pylint")
+(load-library "pylint-flymake")
