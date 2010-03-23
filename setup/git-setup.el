030116fc919052e8e1cd15f2a54668cc28727ffd
knevcher <knevcher@gmail.com>
2010-03-23 12:02:25 +0300

    -- added gitsum repository as submodule

--- setup/git-setup.el
+++ setup/git-setup.el
@@ -1,6 +1,10 @@
+(add-to-list 'load-path "~/.emacs.d/modules/gitsum/")
+
 (global-set-key (kbd "<f4>") 'magit-status)
 
 ;;git-blame
 (require 'git-blame)
 (autoload 'git-blame-mode "git-blame"
   "Minor mode for incremental blame for Git." t)
+(require 'gitsum)
+
