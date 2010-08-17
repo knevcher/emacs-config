0d67dcbabb9556d49dc0e681d76d854cb18e6d07
knevcher <knevcher@gmail.com>
2010-08-17 10:01:26 +0400

    -- added magit repo

--- setup/git-setup.el
+++ setup/git-setup.el
@@ -1,5 +1,7 @@
+(add-to-list 'load-path "~/.emacs.d/modules/magit/")
 (add-to-list 'load-path "~/.emacs.d/modules/gitsum/")
 
+(require 'magit)
 (global-set-key (kbd "<f4>") 'magit-status)
 
 ;;git-blame
