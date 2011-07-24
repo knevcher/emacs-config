ea96a4883ff2e222c96503c4632758ac39d0ab1d
knevcher <knevcher@gmail.com>
2011-07-24 13:36:46 +0400

    -- updated python -- raised org version

--- setup/python-setup.el
+++ setup/python-setup.el
@@ -1,11 +1,33 @@
+(setq-default py-indent-offset 4)
+
+(add-hook 'python-mode-hook
+    (function (lambda ()
+                (setq indent-tabs-mode nil)
+                (setq c-indent-level 4))))
+
+(add-hook 'python-mode-hook
+  (function (lambda ()
+    (setq indent-tabs-mode nil
+       tab-width 4))))
 
 (add-to-list 'load-path "~/.emacs.d/modules/python/")
 (add-to-list 'load-path "~/.emacs.d/modules/pymacs")
+(add-to-list 'load-path "~/.emacs.d/elpa/guess-style-0.1/")
 
 (autoload 'python-mode "python-mode" "Python Mode." t)
 (require 'python-mode)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
+
+(require 'guess-style-autoloads)
+(require 'guess-style)
+
+(autoload 'guess-style-set-variable "guess-style" nil t)
+(autoload 'guess-style-guess-variable "guess-style")
+(autoload 'guess-style-guess-all "guess-style" nil t)
+
+(add-hook 'python-mode-hook 'guess-style-guess-all)
+(global-guess-style-info-mode 1)
 
 ;;(require 'python-init)
 (require 'ipython)
