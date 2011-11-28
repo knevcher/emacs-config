3fbcf52d9966db44a1e785d726e92fa76abf4df7
knevcher <knevcher@gmail.com>
2011-11-28 16:27:57 +0400

    -- added slime repo

--- setup/slime-setup.el
+++ setup/slime-setup.el
@@ -1,7 +1,9 @@
-(add-to-list 'load-path "~/.emacs.d/elpa/slime-20100404/")
+(add-to-list 'load-path "~/.emacs.d/modules/slime/")
 (require 'slime-autoloads)
 
 (setq inferior-lisp-program "/usr/bin/sbcl")
 (require 'slime)
 (slime-setup)
+
+;;(swank:create-server :port 4005 :style :spawn :dont-close t)
 
